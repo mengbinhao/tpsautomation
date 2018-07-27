1ff461b9191e887b82f6a56aa0a98bbcff92fff9
Jack Meng <mengbinhao2003@126.com>
2018-07-27 18:06:58 +0800

    minor change

--- position.py
+++ position.py
@@ -1,10 +1,11 @@
+''' display cursor position'''
 import pyautogui
 print('Press Ctrl-C to quit')
 try:
     while True:
         x, y = pyautogui.position()
-        positionStr = 'X: {} Y: {}'.format(*[str(x).rjust(4) for x in [x, y]])
-        print(positionStr, end='')
-        print('\b' * len(positionStr), end='', flush=True)
+        position_str = 'X: {} Y: {}'.format(*[str(x).rjust(4) for x in [x, y]])
+        print(position_str, end='')
+        print('\b' * len(position_str), end='', flush=True)
 except KeyboardInterrupt:
     print('\n')
